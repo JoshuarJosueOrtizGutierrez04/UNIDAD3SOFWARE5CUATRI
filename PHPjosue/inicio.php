46c9b4de8befe627b4fc0a89f0a6e4af008a62e4
JoshuarJosueOrtizGutierrez04 <156556659+JoshuarJosueOrtizGutierrez04@users.noreply.github.com>
2024-03-22 22:31:44 -0700

    FORMULARIO EN PHP

---
+++ PHPjosue/inicio.php
@@ -0,0 +1,4 @@
+<h1>Josue <span class="rojo">Ortiz</span>
+</h1><p>Programador web</p>
+<a href="" download="img/CVSOFTWARE.pdf">
+<button class="boton">Descargar CV</button></a>     
---
+++ PHPjosue/servicios.php
@@ -0,0 +1,8 @@
+<h1><span class="rojo">Servicios</span></h1>
+<nav><ul><li><p>Creacion de programas en c++ y java</p>
+</li><li><p>Creacion de paginas web con css y javascrip</p></li><li><p>creacion de base de datos con mysql</p>
+</li></ul></nav><div class="contenedor-imagenes">
+<img src="img/c++.png" class="imagen" alt="">
+<img src="img/mysql.png" class="imagen" alt="">
+<img src="img/java2.png" class="imagen" alt="">
+<img src="img/java.png" class="imagen" alt=""></div>
